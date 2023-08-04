957d44c5abb7fb884dab02e2f9bb0f2c4bceed39
Игорь <hardplayer455@gmail.com>
2023-08-04 06:44:33 +0300

    3.2. Загрузка файлов на github [#243562]

--- src/main/java/ru/job4j/condition/TrgArea.java
+++ src/main/java/ru/job4j/condition/TrgArea.java
@@ -3,11 +3,7 @@ package ru.job4j.condition;
 public class TrgArea {
     public static double area(double a, double b, double c) {
         double halfP = (a + b + c) / 2;
-        double first = halfP - a;
-        double second = halfP - b;
-        double third = halfP - c;
-        double fourth = first * second * third * halfP;
-        double rsl = Math.sqrt(fourth);
+        double rsl = Math.sqrt((halfP - a) * (halfP - b) * (halfP - c) * halfP);
         return rsl;
     }
 
