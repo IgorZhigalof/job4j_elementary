3a63176c380dd9d78cb8ecf604c03153d657e849
Игорь <hardplayer455@gmail.com>
2023-08-05 15:55:56 +0300

    3.2. Загрузка файлов на github [#243562]

---
+++ src/test/java/ru/job4j/condition/PointTest.java
@@ -0,0 +1,52 @@
+package ru.job4j.condition;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.*;
+import static ru.job4j.condition.Point.*;
+
+class PointTest {
+    @Test
+    void when00to20then2() {
+        double expected = 2;
+        int x1 = 0;
+        int y1 = 0;
+        int x2 = 2;
+        int y2 = 0;
+        double out = Point.distance(x1, y1, x2, y2);
+        assertThat(out).isEqualTo(expected, withPrecision(0.01));
+    }
+
+    @Test
+    void when16to59then5() {
+        int x1 = 1;
+        int y1 = 6;
+        int x2 = 5;
+        int y2 = 9;
+        double expected = 5;
+        double out = distance(x1, y1, x2, y2);
+        assertThat(out).isEqualTo(expected, withPrecision(0.01));
+    }
+
+    @Test
+    void whenX53Y12toX59Y20then10() {
+        int x1 = 53;
+        int y1 = 12;
+        int x2 = 59;
+        int y2 = 20;
+        double expected = 10;
+        double out = distance(x1, y1, x2, y2);
+        assertThat(out).isEqualTo(expected, withPrecision(0.01));
+    }
+
+    @Test
+    void when16to59then10() {
+        int x1 = 1;
+        int y1 = 3;
+        int x2 = 9;
+        int y2 = 9;
+        double expected = 10;
+        double out = distance(x1, y1, x2, y2);
+        assertThat(out).isEqualTo(expected, withPrecision(0.01));
+    }
+
+}
