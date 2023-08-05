580e92bbba93c4a2a6af2ad4f1dbdd9048983910
Игорь <hardplayer455@gmail.com>
2023-08-05 16:20:12 +0300

    3.2. Загрузка файлов на github [#243562]

---
+++ src/test/java/ru/job4j/condition/SqAreaTest.java
@@ -0,0 +1,34 @@
+package ru.job4j.condition;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.*;
+
+class SqAreaTest {
+
+    @Test
+    void whenP6K2SquareThen2() {
+        int expected = 2;
+        int p = 6;
+        double k = 2;
+        double out = SqArea.square(p, k);
+        assertThat(out).isEqualTo(expected, withPrecision(0.01));
+    }
+
+    @Test
+    void whenP2K1SquareThen0dot25() {
+        double expected = 0.25;
+        int p = 2;
+        double k = 1;
+        double out = SqArea.square(p, k);
+        assertThat(out).isEqualTo(expected, withPrecision(0.01));
+    }
+
+    @Test
+    void whenP8K3SquareThen3() {
+        int expected = 3;
+        int p = 8;
+        double k = 3;
+        double out = SqArea.square(p, k);
+        assertThat(out).isEqualTo(expected, withPrecision(0.01));
+    }
+}
