11a053a752c2847491f061a76a75dae13e9a75c8
Игорь <hardplayer455@gmail.com>
2023-08-07 20:25:34 +0300

    Ignored Files

--- src/main/java/ru/job4j/array/SkipNegative.java
+++ src/main/java/ru/job4j/array/SkipNegative.java
@@ -11,13 +11,4 @@ public class SkipNegative {
         }
         return array;
     }
-
-    public static void main(String[] args) {
-        int[][] array = {
-                {1, 2, 3},
-                {3, 2, 1},
-                {2, 3, 1}
-        };
-        SkipNegative.skip(array);
-    }
 }
