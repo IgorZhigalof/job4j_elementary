3b23c40d016ef212d9ba4abc1de8d6ee66136c6e
Игорь <hardplayer455@gmail.com>
2023-08-08 10:43:16 +0300

    Ignored Files

--- src/main/java/ru/job4j/array/Check.java
+++ src/main/java/ru/job4j/array/Check.java
@@ -3,11 +3,10 @@ package ru.job4j.array;
 public class Check {
     public static boolean mono(boolean[] data) {
         boolean result = true;
-        boolean firstElement = data[0];
-        for (boolean element : data) {
-            if (element != firstElement) {
+        for (int i = 0; i < data.length; i++) {
+            if (data[i] != data[0]) {
                 result = false;
-               break;
+                break;
             }
         }
         return result;
