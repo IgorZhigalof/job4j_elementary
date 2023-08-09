845d82f619396e626edb4ee8ad03f932a9640a8a
Игорь <hardplayer455@gmail.com>
2023-08-09 20:10:02 +0300

    Ignored Files

--- src/main/java/ru/job4j/array/Machine.java
+++ src/main/java/ru/job4j/array/Machine.java
@@ -7,16 +7,14 @@ public class Machine {
         int[] coins = {10, 5, 2, 1};
         int[] rsl = new int[100];
         int size = 0;
-        while (money - price > 0) {
-            for (int coin : coins) {
-                if (money - price - coin >= 0) {
-                    rsl[size] = coin;
-                    size++;
-                    money -= coin;
-                    break;
-                }
+        for (int coin : coins) {
+            while (money - price - coin >= 0) {
+                money -= coin;
+                rsl[size] = coin;
+                size++;
             }
         }
         return Arrays.copyOf(rsl, size);
     }
 }
+
