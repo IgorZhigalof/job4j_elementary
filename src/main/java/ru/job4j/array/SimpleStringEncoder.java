6520e298af6873e23471fff8974d460a48e71314
Игорь <hardplayer455@gmail.com>
2023-08-11 13:21:53 +0300

    Ignored Files

--- src/main/java/ru/job4j/array/SimpleStringEncoder.java
+++ src/main/java/ru/job4j/array/SimpleStringEncoder.java
@@ -6,19 +6,15 @@ public class SimpleStringEncoder {
         char symbol = input.charAt(0);
         int counter = 1;
         for (int i = 1; i < input.length(); i++) {
-            if (symbol == input.charAt(i) && i < input.length() - 1) {
+            if (symbol == input.charAt(i)) {
                 counter++;
-            } else if (symbol != input.charAt(i) && i < input.length() - 1) {
-                result += String.valueOf(symbol) + (counter != 1 ?  counter : "");
+            } else {
+                result = counter == 1 ? result + symbol : result + symbol + counter;
                 symbol = input.charAt(i);
                 counter = 1;
-            } else if (symbol != input.charAt(i)) {
-                result += String.valueOf(symbol) + (counter != 1 ?  counter : "") + input.charAt(i);
-            } else if (symbol == input.charAt(i)) {
-                counter++;
-                result += String.valueOf(input.charAt(i)) + counter;
             }
         }
+        result = counter == 1 ? result + symbol : result + symbol + counter;
         return result;
     }
 }
