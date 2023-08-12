5dff63f367c9e50433ea96cdb19d09108ae82e14
Игорь <hardplayer455@gmail.com>
2023-08-12 15:19:47 +0300

    Ignored Files

--- src/main/java/ru/job4j/condition/Max.java
+++ src/main/java/ru/job4j/condition/Max.java
@@ -1,8 +1,15 @@
 package ru.job4j.condition;
 
 public class Max {
-    public static int max(int left, int right) {
-        int result = left > right ? left : right;
-        return result;
+    public int max(int left, int right) {
+        return left > right ? left : right;
+    }
+
+    public int max(int left, int middle, int right) {
+        return max(left, max(middle, right));
+    }
+
+    public int max(int lefter, int left, int right, int righter) {
+        return max(max(lefter, left), max(righter, right));
     }
 }
--- src/test/java/ru/job4j/condition/MaxTest.java
+++ src/test/java/ru/job4j/condition/MaxTest.java
@@ -6,37 +6,64 @@ import static org.assertj.core.api.Assertions.assertThat;
 class MaxTest {
     @Test
     void whenMax1To2Then2() {
+        Max object = new Max();
         int left = 1;
         int right = 2;
-        int result = Max.max(left, right);
+        int result = object.max(left, right);
         int expected = 2;
         assertThat(result).isEqualTo(expected);
     }
 
     @Test
     void whenMax5To2Then5() {
+        Max object = new Max();
         int left = 5;
         int right = 2;
-        int result = Max.max(left, right);
+        int result = object.max(left, right);
         int expected = 5;
         assertThat(result).isEqualTo(expected);
     }
 
     @Test
     void whenMax1To20Then2() {
+        Max object = new Max();
         int left = 1;
         int right = 20;
-        int result = Max.max(left, right);
+        int result = object.max(left, right);
         int expected = 20;
         assertThat(result).isEqualTo(expected);
     }
 
     @Test
     void whenMax2To2Then2() {
+        Max object = new Max();
         int left = 2;
         int right = 2;
-        int result = Max.max(left, right);
+        int result = object.max(left, right);
         int expected = 2;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenMax20To2To6Then20() {
+        Max object = new Max();
+        int left = 20;
+        int middle = 2;
+        int right = 6;
+        int result = object.max(left, middle, right);
+        int expected = 20;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenMax20To2To6To10Then20() {
+        Max object = new Max();
+        int lefter = 20;
+        int left = 2;
+        int right = 6;
+        int righter = 10;
+        int result = object.max(lefter, left, right, righter);
+        int expected = 20;
         assertThat(result).isEqualTo(expected);
     }
 }
