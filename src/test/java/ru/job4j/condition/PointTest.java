c252907b5b6e293bcb1a9f151b10bbf9722d65e8
Игорь <hardplayer455@gmail.com>
2023-08-12 15:39:42 +0300

    Ignored Files

--- src/test/java/ru/job4j/condition/PointTest.java
+++ src/test/java/ru/job4j/condition/PointTest.java
@@ -33,11 +33,29 @@ class PointTest {
     }
 
     @Test
-    void when16to59then10() {
+    void when13to99then10() {
         Point first = new Point(1, 3);
         Point second = new Point(9, 9);
         double expected = 10;
         double out = first.distance(second);
+        assertThat(out).isEqualTo(expected, withPrecision(0.01));
+    }
+
+    @Test
+    void when135to991then10dot77() {
+        Point first = new Point(1, 3, 5);
+        Point second = new Point(9, 9, 1);
+        double expected = 10.77;
+        double out = first.distance3d(second);
+        assertThat(out).isEqualTo(expected, withPrecision(0.01));
+    }
+
+    @Test
+    void when666to999then5dot19() {
+        Point first = new Point(6, 6, 6);
+        Point second = new Point(9, 9, 9);
+        double expected = 5.19;
+        double out = first.distance3d(second);
         assertThat(out).isEqualTo(expected, withPrecision(0.01));
     }
 
